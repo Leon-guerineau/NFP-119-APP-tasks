9f7a0db8a0fea4461326509a14bc77a88d1f12a2
Léon Guérineau <l-guerineau@sfi.fr>
2023-04-24 11:59:18 +0200

    refacto UserList

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,11 +1,26 @@
-import { FC } from "react";
-import ListUsers from '../components/User/ListUsers';
+import {FC, useEffect, useState} from "react";
+import User from "../types/User";
+import {getUsers} from "../services/user.service";
+import UserCreateButton from "../components/User/UserCreateButton";
+import UserTable from "../components/User/UserTable";
 
 const UserList: FC = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    // Récupération des utilisateurs
+    useEffect(() => {
+        async function getData() {
+            const users: User[] = await getUsers();
+            setUsers(users);
+        }
+        getData();
+    });
+
     return (
         <div>
             <h2>Liste des utilisateurs</h2>
-            <ListUsers/>
+            <UserCreateButton/>
+            <UserTable users={users}/>
         </div>
     )
 };
