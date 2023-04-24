95d90aafc1945132c4405642dfcf9692ed3f3c06
Léon Guérineau <l-guerineau@sfi.fr>
2023-04-24 12:15:55 +0200

    refacto UserUpdateButton

--- src/components/User/UserTable.tsx
+++ src/components/User/UserTable.tsx
@@ -1,28 +1,14 @@
-import {FC, useState} from 'react';
-import {IoPencilSharp, IoNewspaperSharp} from 'react-icons/io5';
-import {updateUser} from '../../services/user.service';
+import {FC} from 'react';
+import {IoNewspaperSharp} from 'react-icons/io5';
 import User from "../../types/User";
-import Modal from '../Modal';
-import UserForm from './UserForm';
 import {Link} from "react-router-dom";
 import UserDeleteButton from "./UserDeleteButton";
+import UserUpdateButton from "./UserUpdateButton";
 
 interface Props {
     users: User[];
 }
 const UserTable: FC<Props> = ({users}:Props) => {
-    const [isOpenUpdateForm, setOpenUpdateForm] = useState('');
-
-    // Mise à jour d'un utilisateur
-    const sendUpdateUser = (formData: any) => {
-        setOpenUpdateForm('');
-        const update = async (userUpdate: User) => {
-            await updateUser(userUpdate);
-            window.location.reload();
-        }
-        update(formData);
-    }
-
     // Affichage s'il n'y a aucun utilisateur
     if (users.length === 0) {
         return (<h1>Aucun utilisateur</h1>)
@@ -39,33 +25,24 @@ const UserTable: FC<Props> = ({users}:Props) => {
                 </tr>
             </thead>
             <tbody>
-            {users?.map((user, key) => {
-                return (
-                    <tr key={key}>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>
-                            <Link to={'/users/' + user._id + '/tasks'}>
-                                <button className='iconButton' title="Lister les taĉhes">
-                                    <IoNewspaperSharp/>
-                                </button>
-                            </Link>
-
-                            <button className='iconButton' onClick={() => setOpenUpdateForm(user._id)} title="Modifier">
-                                <IoPencilSharp/>
-                            </button>
-                            <Modal
-                                isOpen={isOpenUpdateForm === user._id}
-                                onClose={() => setOpenUpdateForm('')}
-                                title="Modifier un utilisateur"
-                                content={<UserForm onSubmit={sendUpdateUser} user={user}/>}
-                            />
-
-                            <UserDeleteButton user={user}/>
-                        </td>
-                    </tr>
-                )
-            })}
+                {/* Affichage des lignes d'utilisateurs */}
+                {users?.map((user, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>
+                                <Link to={'/users/' + user._id + '/tasks'}>
+                                    <button className='iconButton' title="Lister les taĉhes">
+                                        <IoNewspaperSharp/>
+                                    </button>
+                                </Link>
+                                <UserUpdateButton user={user}/>
+                                <UserDeleteButton user={user}/>
+                            </td>
+                        </tr>
+                    )
+                })}
             </tbody>
         </table>
     )
